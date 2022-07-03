8f2b6cb2ec889b196af37f6d85db61854e64505e
Rob Cole <rob-cole@hotmail.co.uk>
2022-07-03 12:54:21 +0000

    Add admin panel models for Category app

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Category
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_filter = ['name']
+    list_display = ['name']
+    search_fields = ['name']
--- topic/admin.py
+++ topic/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import Topic
 
 
-@admin.register()
-
-class AdminTopic(admin.ModelAdmin):
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
     list_filter = ['category', 'title', 'author', 'closed']
     list_display = ['category', 'title', 'author', 'closed']
     search_fields = ['category', 'title', 'author']
