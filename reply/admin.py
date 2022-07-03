5d4729d161c8fb7b6884f68b9053d4e6214f1f82
Rob Cole <rob-cole@hotmail.co.uk>
2022-07-03 12:46:37 +0000

    Add admin panel models for Reply app

--- reply/admin.py
+++ reply/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Reply
 
-admin.site.register(Reply)
+
+@admin.register(Reply)
+class ReplyAdmin(admin.ModelAdmin):
+    """
+    Settings for filter, display and search
+    """
+    list_filter = ['topic', 'author', 'replydate']
+    list_display = ['topic', 'author', 'replydate']
+    search_fields = ['topic', 'author']
