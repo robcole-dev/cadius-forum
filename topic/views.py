f7552d0f6f7b3f0cca9774a45c2d76ad391f8b2a
Rob Cole <rob-cole@hotmail.co.uk>
2022-07-11 18:03:41 +0000

    Remove obsolete code from topic views.py

--- topic/views.py
+++ topic/views.py
@@ -4,22 +4,6 @@ from category.models import Category
 from .models import Topic
 
 
-# class TopicList(View):
-
-#     def get(self, request, category, *args, **kwargs):
-
-#         queryset = Topic.objects.filter(category=category)
-#         topic = get_object_or_404(queryset)
-
-#         return render(
-#             request,
-#             "topic_detail.html",
-#             {
-#                 "title": topic
-#             },
-#         )
-
-
 def topic_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     topics = Topic.objects.filter(category=category)
