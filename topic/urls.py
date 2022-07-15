ee89419d41f38c493039dab776fa2a9b7a860808
Rob Cole <rob-cole@hotmail.co.uk>
2022-07-15 11:14:05 +0000

    Add delete topic functionality

--- topic/urls.py
+++ topic/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
         'category/<int:category_id>/',
         views.category_detail, name="category_detail"),
     path('new/<int:category_id>/', views.new_topic, name="new_topic"),
+    path(
+        'edit/category/<int:category_id>/topic/<int:topic_id>/',
+        views.edit_topic, name="edit_topic"),
+    path(
+        'delete/category/<int:category_id>/topic/<int:topic_id>/',
+        views.delete_topic, name="delete_topic"),
 ]
--- topic/views.py
+++ topic/views.py
@@ -30,6 +30,7 @@ def new_topic(request, category_id):
             topic_id = topic.pk
             messages.success(request, 'Topic Added')
             return redirect(reverse('topic_detail', args=[category_id, topic_id]))
+        messages.error(request, "Try again!")
 
     template = "topic/new_topic.html"
     context = {
@@ -37,3 +38,41 @@ def new_topic(request, category_id):
         "topic_form": topic_form,
     }
     return render(request, template, context)
+
+
+@login_required
+def edit_topic(request, category_id, topic_id):
+    category = get_object_or_404(Category, id=category_id)
+    topic = get_object_or_404(Topic, id=topic_id)
+    if topic.author != request.user:
+        messages.error(request, "You are not the author!")
+        return redirect('home')
+    topic_form = TopicForm(request.POST or None, instance=topic)
+    if request.method == 'POST':
+        if topic_form.is_valid():
+            topic_form.instance.author = request.user
+            topic_form.instance.category = category
+            topic_form.save()
+            messages.success(request, 'Topic Updated')
+            return redirect(reverse('topic_detail', args=[category_id, topic_id]))
+        messages.error(request, "Try again!")
+
+    template = "topic/edit_topic.html"
+    context = {
+        "category": category,
+        "topic": topic,
+        "topic_form": topic_form,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def delete_topic(request, category_id, topic_id):
+    category = get_object_or_404(Category, id=category_id)
+    topic = get_object_or_404(Topic, id=topic_id)
+    if topic.author != request.user:
+        messages.error(request, "You are not the author!")
+        return redirect('home')
+    topic.delete()
+    messages.success(request, 'Topic deleted')
+    return redirect(reverse('category_detail', args=[category_id]))
