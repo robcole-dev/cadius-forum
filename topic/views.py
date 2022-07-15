24c366437b956ec827d814b659f6d8d1e2c23a25
Rob Cole <rob-cole@hotmail.co.uk>
2022-07-15 10:19:31 +0000

    Add crud add functionality for new topic

--- topic/views.py
+++ topic/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import View
+from datetime import date
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from category.models import Category
 from .models import Topic
 from .forms import TopicForm
-from datetime import date
 
 
-def topic_detail(request, category_id):
+def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     topics = Topic.objects.filter(category=category)
-    template = "topic_detail.html"
+    template = "topic/category_detail.html"
     context = {
         "category": category,
         "topics": topics,
@@ -17,36 +18,22 @@ def topic_detail(request, category_id):
     return render(request, template, context)
 
 
+@login_required
 def new_topic(request, category_id):
     category = get_object_or_404(Category, id=category_id)
-    topics = Topic.objects.filter(category=category)
-    template = "new_topic.html"
+    topic_form = TopicForm(request.POST or None)
+    if request.method == 'POST':
+        if topic_form.is_valid():
+            topic_form.instance.author = request.user
+            topic_form.instance.category = category
+            topic = topic_form.save()
+            topic_id = topic.pk
+            messages.success(request, 'Topic Added')
+            return redirect(reverse('topic_detail', args=[category_id, topic_id]))
+
+    template = "topic/new_topic.html"
     context = {
         "category": category,
-        "topics": topics,
-        "topic_form": TopicForm(),
+        "topic_form": topic_form,
     }
     return render(request, template, context)
-
-
-def transact(self, request, category_id):
-    category = get_object_or_404(Category, id=category_id)
-    topics = Topic.objects.filter(category=category)
-
-    topic_form = TopicForm(data=request.post)
-
-    if request.post and topic_form.is_valid():
-        topic_form.instance.author = request.user.username
-        topic = topic_form.save(commit=False)
-        topic.category = category
-        topic.startdate = datetime.now()
-        topic.save()
-    else:
-        topic_form = TopicForm()
-
-    context = {
-        "category": category,
-        "topics": topics,
-    }
-
-    return render(request, context)
