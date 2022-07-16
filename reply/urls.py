b4e8bfba3b29fce0e6031364ae1c40934bfc5108
Rob Cole <rob-cole@hotmail.co.uk>
2022-07-16 15:38:23 +0000

    Add path for edit reply page

--- reply/urls.py
+++ reply/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path(
         'category/<int:category_id>/topic/<int:topic_id>/',
         views.topic_detail, name="topic_detail"),
+    path(
+        'edit/category/<int:category_id>/topic/<int:topic_id>/reply/<int:reply_id>',  # noqa
+        views.edit_reply, name="edit_reply"),
 ]
