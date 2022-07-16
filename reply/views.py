9ac439da4d22ca7f85135bc3cfd41c4b1b28b361
Rob Cole <rob-cole@hotmail.co.uk>
2022-07-16 15:39:42 +0000

    Add creation of reply and edit reply functionality

--- reply/views.py
+++ reply/views.py
@@ -1,18 +1,64 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import View
+from datetime import datetime
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from category.models import Category
 from topic.models import Topic
 from .models import Reply
 from .forms import ReplyForm
 
 
 def topic_detail(request, category_id, topic_id):
+    category = get_object_or_404(Category, id=category_id)
     topic = get_object_or_404(Topic, id=topic_id)
     replys = Reply.objects.filter(topic=topic)
-    template = "reply/topic_reply.html"
+    reply_form = ReplyForm(request.POST or None)
+    if request.method == 'POST':
+        if reply_form.is_valid():
+            reply_form.instance.author = request.user
+            reply_form.instance.category = category
+            reply_form.instance.topic = topic
+            reply_form.save()
+            messages.success(request, "Reply Added")
+            return redirect(reverse(
+                'topic_detail', args=[category_id, topic_id]))
+        messages.error(request, "Try again!")
+
+    template = "reply/topic_detail.html"
     context = {
         "category_id": category_id,
         "topic": topic,
         "replys": replys,
-        "reply_form": ReplyForm(),
+        "reply_form": ReplyForm()
+    }
+    return render(request, template, context)
+
+
+@login_required
+def edit_reply(request, category_id, topic_id, reply_id):
+    category = get_object_or_404(Category, id=category_id)
+    topic = get_object_or_404(Topic, id=topic_id)
+    reply = get_object_or_404(Reply, id=reply_id)
+    if reply.author != request.user:
+        messages.error(request, "You are not the Author!")
+        return redirect('home')
+    reply_form = ReplyForm(request.POST or None, instance=reply)
+    if request.method == 'POST':
+        if reply_form.is_valid():
+            reply_form.instance.author = request.user
+            reply_form.instance.category = category
+            reply_form.instance.topic = topic
+            reply_form.save()
+            messages.success(request, "Reply updated")
+            return redirect(reverse(
+                'topic_detail', args=[category_id, topic_id]))
+        messages.error(request, "Try again!")
+
+    template = "reply/edit_reply.html"
+    context = {
+        "category_id": category_id,
+        "topic": topic,
+        "replys": reply,
+        "reply_form": reply_form,
     }
     return render(request, template, context)
